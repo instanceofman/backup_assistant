4de2509751a7469d39c50ed03994139f61a801b2
Nam Nguyen <instanceofman@gmail.com>
2022-12-16 23:35:39 +0700

    save

--- src/app.js
+++ src/app.js
@@ -3,15 +3,17 @@ const app = express();
 const bodyParser = require("body-parser");
 const http = require("http");
 const server = http.createServer(app);
+const timeout = (timeout = require("connect-timeout"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(timeout("20s"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/backup/mssql", require('./mssql'));
+app.post("/backup/mssql", require("./mssql"));
 
 server.listen(8083, () => {
   console.log("listening on *:8083");
