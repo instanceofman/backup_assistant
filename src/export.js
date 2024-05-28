aaeaf050a822114b38d8453228f40cb6facf1222
Nam Nguyen <instanceofman@gmail.com>
2024-05-28 14:55:09 +0700

    publish

--- src/export.js
+++ src/export.js
@@ -10,6 +10,7 @@ module.exports = async (exportDir, dbConfig) => {
   const fileName = `${dbConfig.database}_${date}.bak`;
   const exportTo = `${exportDir}${fileName}`;
   const query = `BACKUP DATABASE ${dbConfig.database} TO DISK = '${exportTo}'`;
+  const sql = dbConfig.native ? require("mssql/msnodesqlv8") : require("mssql");
 
   dbConfig.port = dbConfig.hasOwnProperty("port") ? dbConfig.port : 1433;
   dbConfig.native = dbConfig.hasOwnProperty("native") ? dbConfig.native : false;
@@ -20,25 +21,24 @@ module.exports = async (exportDir, dbConfig) => {
     database: dbConfig.database,
     server: dbConfig.server,
     port: dbConfig.port,
-    // pool: {
-    //   max: 10,
-    //   min: 0,
-    //   idleTimeoutMillis: 30000,
-    // },
+    connectionTimeout: 360000,
+    requestTimeout: 360000,
+    pool: {
+      max: 10,
+      min: 0,
+      idleTimeoutMillis: 360000,
+    },
     options: {
       encrypt: false,
       trustServerCertificate: false,
+      connectionTimeout: 360000,
     },
   };
-
-  // var sql = dbConfig.native ? require("mssql/msnodesqlv8") : require("mssql");
 
   if (dbConfig.native) {
     sqlConfig.options.trustedConnection = true;
     sqlConfig.driver = "msnodesqlv8";
   }
-
-  const sql = dbConfig.native ? require("mssql/msnodesqlv8") : require("mssql");
 
   await sql.connect(sqlConfig);
   await sql.query(query);
